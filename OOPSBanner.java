db74b90c8ddae416b1b9b33498c32e8c1aed00ff
ts4950 <ts4950@srmist.edu.in>
2026-02-28 18:00:53 +0530

    Created OOPSBanner

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,15 +1,51 @@
 public class OOPSBanner {
-    public static void main(String[] args) {
+    
+    public static String[] getOPattern(){
+        return new String[]{"   ***   ",
+                            " **   ** ",
+                            "**     **",
+                            "**     **",
+                            "**     **",
+                            " **   ** ",
+                            "   ***   "
 
-        System.out.println("    ***        ***      ******     *****     ");
-        System.out.println("  **   **   **     **   **   **   **         ");
-        System.out.println(" **     **  **     **   **    ** **          ");
-        System.out.println(" **     **  **     **   **   **    **        ");
-        System.out.println(" **     **  **     **   ******      ***      ");
-        System.out.println(" **     **  **     **   **            **     ");
-        System.out.println(" **     **  **     **   **             **    ");
-        System.out.println("  **   **    **   **    **            **     ");
-        System.out.println("    ***        ***      **        *****       ");
+        };
+    }
+    public static String[] getPPattern(){
+        return new String[] {" ******  ",
+                             " *    *  ",
+                             " ******  ",
+                             " *       ",
+                             " *       ",
+                             " *       ",
+                             " *       "
 
+        };
+    }
+    public static String[] getSPattern(){
+        return new String[] {"   ***   ",
+                             " **      ",
+                             "  **     ",
+                             "    ***  ",
+                             "       **",
+                             " **    **",
+                             "   ***   "
+
+        };
+    }
+  
+    public static void main(String[] args){
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+        
+        for (int i = 0; i < oPattern.length;i++){
+            System.out.println(
+                oPattern[i]+" "+
+                oPattern[i]+" "+
+                pPattern[i]+" "+
+                sPattern[i]
+            );
+        }
     }
 }
