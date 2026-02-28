47f627b904976d4278c6bc4d1765e1c55eb90490
ts4950 <ts4950@srmist.edu.in>
2026-02-28 17:59:27 +0530

    Create OOPSBanner

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,15 +1,16 @@
 public class OOPSBanner {
     public static void main(String[] args) {
-
-        System.out.println("    ***        ***      ******     *****     ");
-        System.out.println("  **   **   **     **   **   **   **         ");
-        System.out.println(" **     **  **     **   **    ** **          ");
-        System.out.println(" **     **  **     **   **   **    **        ");
-        System.out.println(" **     **  **     **   ******      ***      ");
-        System.out.println(" **     **  **     **   **            **     ");
-        System.out.println(" **     **  **     **   **             **    ");
-        System.out.println("  **   **    **   **    **            **     ");
-        System.out.println("    ***        ***      **        *****       ");
-
+        String[] banner = {
+            String.join(" ", "   ***   ", "   ***    ", " ******  ", "   *****  "),
+            String.join(" ", " **   ** ", " **   **  ", " *    *   ", " **       "),
+            String.join(" ", "**     **", "**     ** ", " ******   ", "**        "),
+            String.join(" ", "**     **", "**     ** ", " *        ", "  *****   "),
+            String.join(" ", "**     **", "**     ** ", " *        ", "      **  "),
+            String.join(" ", " **   ** ", " **   **  ", " *        ", "**    **  "),
+            String.join(" ", "   ***   ", "   ***    ", " *        ", "  *****   ")
+        };
+        for (String line : banner) {
+            System.out.println(line);
+        };
     }
 }
